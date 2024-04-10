611a706168436e5b0be7cf952760a203cecccc0d
Aryan Vij <aryanv0213@gmail.com>
2024-04-09 18:25:13 -0700

    html frontend

--- app.py
+++ app.py
@@ -21,6 +21,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = reg_model.predict(final_input)
+    return render_template("home.html", prediction_text = "The House price prediction is {}".format(output[0]))
+
 if __name__ == "__main__":
     app.run(port=8000, debug=True) #Running the app
     #Run on port 8000 since Mac now runs its control center on port 5000 and 7000 which throws a 403 forbidden error with postman
